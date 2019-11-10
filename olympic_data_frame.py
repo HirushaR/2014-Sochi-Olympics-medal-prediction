435530fb73299c787609b93eb0c3fe4a81a28e68
HirushaR <hirusharandunu11@gmail.com>
2019-11-10 18:05:27 +0530

    datafram 01

--- olympic_data_frame.py
+++ olympic_data_frame.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 contries = ['Russia Fed.', 'Norway', 'Canada', 'United States', 'Netherlands', 'Germany',
             'Switzerland', 'Belarus', 'Austria', 'France', 'Poland', 'Chine', 'Korea',
@@ -9,7 +10,11 @@ Gold = [13, 11, 10, 9, 8, 8, 6, 5, 4, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 
 Silver = [11, 5, 10, 7, 7, 6, 3, 0, 8, 4, 1, 4, 3, 7, 4, 2, 4, 3, 1, 0, 0, 2, 2, 2, 1, 0]
 Bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
 
-olympic_medal_count_df = { 'Country_name' : pd.Series(contries), 'Gold': pd.Series(Gold), 'Silver': pd.Series(Silver), 'Bronze': pd.Series(Bronze)}
+d = {'Country_name' : pd.Series(contries), 'Gold': pd.Series(Gold), 'Silver': pd.Series(Silver), 'Bronze': pd.Series(Bronze)}
 
-df = pd.DataFrame(olympic_medal_count_df)
-print(df)
+df = pd.DataFrame(d)
+c = df[df['Gold'] >= 1]
+#weathe vale is 1 or greate than one
+e = df['Gold'].map(lambda x: x >= 1)
+print(e)
+
